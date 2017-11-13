9d5faca8c7da1695afbbcb68ea1780c6dda700fc
pnadolny13 <pnadolny@deloitte.com>
2017-11-12 23:19:48 -0500

    Method parsing and exception thrown
    bracket tracking to parse methods better and added throws exception in method declaration.

--- src/codeviz/Start.java
+++ src/codeviz/Start.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.apache.commons.lang3.StringUtils;
 
 public class Start {
 	public Start() {
@@ -34,27 +35,23 @@ public class Start {
 		// copy src dir to temp dir
 		FileUtils.copyDirectory(srcFileDir, tempFileDir);
 
-		
 		// create CodeViz log file to put all code output
 		Random rand = new Random();
-		String codeVizLogFile =  codeVizLogDir + "\\CodeVizLog_" + versionID + ".txt";
+		String codeVizLogFile = codeVizLogDir + "\\CodeVizLog_" + versionID + ".txt";
 		File codeVizLogs = new File(codeVizLogFile);
 		codeVizLogs.createNewFile();
-		
-		
+
 		// validate copy happened then kick off the updating of code
 
 		iterateDirectory(tempFileDir, codeVizLogFile);
-		
+
 		// change src to tempFile name plus original
-		File orignalSrc_temp = new File(srcPath+ "_original_" + versionID);
+		File orignalSrc_temp = new File(srcPath + "_original_" + versionID);
 		File codeVizSrcVersion = new File(srcPath);
 		srcFileDir.renameTo(orignalSrc_temp);
 
-		
 		// change tempFile name to srcFile name
-		tempFileDir.renameTo(codeVizSrcVersion);		
-		
+		tempFileDir.renameTo(codeVizSrcVersion);
 
 	}
 
@@ -77,10 +74,12 @@ public class Start {
 					try {
 						String className = "";
 						String methodName = "";
+						int bracketCount = 0;
 						while (it.hasNext()) {
 							String line = it.nextLine();
 							// find class declaration
-							if (line.contains("public class") || line.contains("protected class") || line.contains("private class")) {
+							if (line.contains("public class") || line.contains("protected class")
+									|| line.contains("private class")) {
 								String[] contents = line.split(" ");
 								for (int i = 0; i < contents.length; i++) {
 									// System.out.println("class contents " + contents[i]);
@@ -96,6 +95,8 @@ public class Start {
 								wr.newLine();
 								wr.write("import java.io.FileWriter;");
 								wr.newLine();
+								wr.write("import java.io.IOException;");
+								wr.newLine();
 								wr.newLine();
 								// add throws IOException before the close bracket
 								wr.write(line);
@@ -103,33 +104,48 @@ public class Start {
 								wr.write(codeVizLine);
 								wr.newLine();
 							}
-							// if the line is a method, set method name
-							else if (line.contains("method1")) {
-								methodName = "method1";
-								String codeVizLine = "\"" + methodName + " from " + className + " was executed" + "\"";
-								// add throws IOException before the close bracket
-								wr.write(line);
-								wr.newLine();
-								wr.write("        // Auto-Populated CodeViz Code");
-								wr.newLine();
-								wr.write("        File codeVizLogFile = new File(\"" + codeVizLogFile.toString().replace("\\", "\\\\") + "\") ;");
-								wr.newLine();
-								wr.write("        BufferedWriter wr = new BufferedWriter(new FileWriter(codeVizLogFile, true));");
-								wr.newLine();
-								wr.write("        wr.write(" + codeVizLine + ");");
-								wr.newLine();
-								wr.write("        wr.newLine();");
-								wr.newLine();
-								wr.write("        wr.close();");
-								wr.newLine();
+							// if the line is a method, set method name ... close bracket means the class is closing
+							else if (bracketCount == 1 && !line.contains("}")) {
+								// this is a method line...extract
+								String[] methodLineContents = line.split("\\(");
+								String[] method = methodLineContents[0].split(" ");
+								String actualmethod = method[method.length - 1];
+								methodName = StringUtils.remove(actualmethod, "()");
 
-
-							} else {
+								// if its the same as class name, its a contructor so dont add codeViz line
+								if (!methodName.equals(className)) {
+									String codeVizLine = "\"" + methodName + " from " + className + " was executed" + "\"";
+									// add throws IOException before the close bracket
+									wr.write(line.replace("{", " throws IOException {"));
+									wr.newLine();
+									wr.write("        // Auto-Populated CodeViz Code");
+									wr.newLine();
+									wr.write("        File codeVizLogFile = new File(\"" + codeVizLogFile.toString().replace("\\", "\\\\") + "\") ;");
+									wr.newLine();
+									wr.write("        BufferedWriter wr = new BufferedWriter(new FileWriter(codeVizLogFile, true));");
+									wr.newLine();
+									wr.write("        wr.write(" + codeVizLine + ");");
+									wr.newLine();
+									wr.write("        wr.newLine();");
+									wr.newLine();
+									wr.write("        wr.close();");
+									wr.newLine();
+								}
+							}
+							else {
 								wr.write(line);
 								wr.newLine();
 							}
-
+							if (line.contains("{")) {
+								int ups = StringUtils.countMatches(line, "{");
+								bracketCount += ups;
+							}
+							if (line.contains("}")) {
+								int downs = StringUtils.countMatches(line, "}");
+								bracketCount -= downs;
+							}
 						}
+
 					} finally {
 						it.close();
 						wr.close();
