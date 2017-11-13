f22cfa6b52c34e41d4f7b94ce776a925910c5028
pnadolny13 <pnadolny@deloitte.com>
2017-11-12 21:53:12 -0500

    test2

--- src/codeviz/ConnectToSrc.java
+++ src/codeviz/ConnectToSrc.java
@@ -8,7 +8,6 @@ public class ConnectToSrc {
 
 	public ConnectToSrc() {
 		// TODO Auto-generated constructor stub
-		// added to make update
 	}
 
 	public void execute(String command, String path) throws Exception {
