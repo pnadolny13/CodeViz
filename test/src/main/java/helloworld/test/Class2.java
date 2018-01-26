7748a74db2598dc94ae4fbf479e05316af7914ad
pnadolny13 <pnadolny@deloitte.com>
2018-01-25 20:07:10 -0500

    Re-organize to include test

---
+++ test/src/main/java/helloworld/test/Class2.java
@@ -0,0 +1,13 @@
+package helloworld.test;
+
+public class Class2 {
+
+	public Class2(String something){
+		System.out.println(something);
+		method1("hello from class2 method1");
+		
+	}
+	public void method1(String print) {
+		System.out.println(print);
+	}
+}
